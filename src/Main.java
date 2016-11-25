f2f6a57bfe1f84eee70a8ec7f6e0b9c0c4d66de4
Tomer Ben Kereth <bk.tomer@gmail.com>
2016-11-25 17:09:46 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Main {
 	
 	
+	private static final int MAT_SIZE = 3;
+
+
+
 	public static void main(String[] args) {
 		
 		String inputMatrixFileName = args[0];
@@ -37,8 +47,15 @@ public class Main {
 
 
 
-	private static Matrix parseMatrixFile(String inputMatrixFileName) {
-		// TODO Auto-generated method stub
+	private static Matrix parseMatrixFile(String inputMatrixFileName) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(new File(inputMatrixFileName)));
+		
+		String matString = br.readLine();
+		
+		String[] MatLines = mat.split(";");
+		
+		Fraction[][] matrix = new Fraction[MAT_SIZE][MAT_SIZE];
+		
 		return null;
 	}
 

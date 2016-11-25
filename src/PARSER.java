fb37488ec441279f9170b11d7be96963d7ba517f
Tomer Ben Kereth <bk.tomer@gmail.com>
2016-11-25 15:43:54 +0200

    changed from Frac to Fraction

--- src/PARSER.java
+++ src/PARSER.java
@@ -1,44 +1,51 @@
 
 public class PARSER {
-	class Frac {
+	
+	private class Fraction {
 		int numerator;
 		int denominator;
-		
-		Frac(int numerator, int denominator) {
+
+		private Fraction(int numerator, int denominator) {
 			this.numerator = numerator;
 			this.denominator = denominator;
 		}
-		
-		Frac multiply(Frac y) {
-			
-			return (new Frac(this.numerator * y.numerator, this.denominator * y.denominator)).simplify();
+
+		private Fraction multiply(Fraction y) {
+
+			return (new Fraction(this.numerator * y.numerator, this.denominator * y.denominator)).simplify();
 		}
-		
-		Frac addition(Frac y) {
+
+		private Fraction addition(Fraction y) {
 			int commonDenominator = this.denominator * y.denominator;
 			int newNumenator = this.numerator * y.denominator + y.numerator * this.denominator;
-			return (new Frac(newNumenator,commonDenominator)).simplify();
+			return (new Fraction(newNumenator,commonDenominator)).simplify();
 		}
-		
-		Frac subtraction(Frac y) {
+
+		private Fraction subtraction(Fraction y) {
 			int commonDenominator = this.denominator * y.denominator;
 			int newNumenator = this.numerator * y.denominator - y.numerator * this.denominator;
-			return (new Frac(newNumenator,commonDenominator)).simplify();
+			return (new Fraction(newNumenator,commonDenominator)).simplify();
 		}
-		
-		Frac simplify() {
+
+		private Fraction simplify() {
 			int gcd = GCD(this.numerator, this.denominator);
 			this.numerator /= gcd;
 			this.denominator /= gcd;
 			return this;
 		}
-		
-		int GCD(int a, int b) { return b==0 ? a : GCD(b, a%b); }
+
+		private int GCD(int a, int b) {
+			return b==0 ? a : GCD(b, a%b);
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
 }
+
+
+
+
